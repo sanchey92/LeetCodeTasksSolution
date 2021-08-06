401ec28ebe86e114f9fd3fad0e8629bbab83fbc3
alexandr <sashkaageev@gmail.com>
2021-08-06 03:23:40 +0300

    Added solution for duplicates in array

--- LeetCodeTasks.Library/Duplicates.cs
+++ LeetCodeTasks.Library/Duplicates.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+
 namespace LeetCodeTasks.Library
 {
-    public class Duplicates
+    public static class Duplicates
     {
-        
+        public static bool HasDuplicates(IEnumerable<int> nums)
+        {
+            var hashSet = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (hashSet.Contains(num))
+                    return true;
+                else
+                    hashSet.Add(num);
+            }
+
+            return false;
+        }
     }
 }
--- LeetCodeTasks.Tests/DuplicatesTest.cs
+++ LeetCodeTasks.Tests/DuplicatesTest.cs
@@ -1,7 +1,25 @@
+using LeetCodeTasks.Library;
+using Xunit;
+
 namespace LeetCodeTasks.Tests
 {
     public class DuplicatesTest
     {
-        
+        [Fact]
+        public void HasDuplicates()
+        {
+            // Arrange
+            var nums1 = new[] { 1, 2, 3, 1 };
+            var nums2 = new[] { 1, 2, 3, 4 };
+            var nums3 = new[] { 0, 2, 4, 6, 2, 0 };
+            // Act
+            var result1 = Duplicates.HasDuplicates(nums1);
+            var result2 = Duplicates.HasDuplicates(nums2);
+            var result3 = Duplicates.HasDuplicates(nums3);
+            // Assert
+            Assert.True(result1);
+            Assert.False(result2);
+            Assert.True(result3);
+        }
     }
 }
