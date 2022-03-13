9b5566870d260c2576737d0c142ac6efa1f046ce
Alexandr Ageev <sashkaageev@gmail.com>
2022-03-13 18:39:04 +0300

    Added Array Left Notation

---
+++ LeetCodeTasks.Library/ArrayLeftRotation.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeTasks.Library
+{
+    public static class ArrayLeftRotation
+    {
+        public static int[] GetLeftRotation(int[] array, int shift)
+        {
+            var length = array.Length;
+            var newArray = new int[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                var index = i - shift;
+                var place = length + index;
+
+                if (index >= 0)
+                {
+                    newArray[index] = array[i];
+                }
+                else
+                {
+                    newArray[place] = array[i];
+                }
+            }
+            
+            return newArray;
+        }
+    }
+}
---
+++ LeetCodeTasks.Tests/GetLeftRotationTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeTasks.Library;
+using Xunit;
+
+namespace LeetCodeTasks.Tests
+{
+    public class GetLeftRotationTests
+    {
+        [Fact]
+        public void Try_Execute_LeftRotation()
+        {
+            // Arrange
+            var array = new int[] {1, 2, 3, 4, 5};
+            // Act
+            var newArray1 = ArrayLeftRotation.GetLeftRotation(array, 4);
+            var newArray2 = ArrayLeftRotation.GetLeftRotation(array, 2);
+            // Assert
+            var tryArray1 = new int[] {5, 1, 2, 3, 4};
+            var tryArray2 = new int[]  {3, 4, 5, 1, 2};
+            Assert.Equal(tryArray1, newArray1);
+            Assert.Equal(tryArray2, newArray2);
+        }
+    }
+}
