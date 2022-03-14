45356de2f348eedf31cc0806bb44e8e555580fb6
Alexandr Ageev <sashkaageev@gmail.com>
2022-03-14 15:49:03 +0300

    Added SubarraySumEqualsK

---
+++ LeetCodeTasks.Library/SubarraySumEqualsK.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCodeTasks.Library
+{
+    public static class SubarraySumEqualsK
+    {
+        // cumulative sum without space
+        public static int GetCumulativeSumCount(int[] array, int k)
+        {
+            var count = 0;
+            var length = array.Length;
+            for (var start = 0; start < length; start++)
+            {
+                var sum = 0;
+                for (var end = start; end < length; end++)
+                {
+                    sum += array[end];
+                    if (sum == k)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ LeetCodeTasks.Tests/SubarraySumCountTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeTasks.Library;
+using Xunit;
+
+namespace LeetCodeTasks.Tests
+{
+    public class SubarraySumCountTests
+    {
+        [Fact]
+        public void Try_Execute_SubarraySumCount()
+        {
+            // Arrange
+            var array1 = new int[] {10, 2, -2, -20, 10};
+            var k1 = -10;
+            var output1 = 3;
+
+            var array2 = new int[] {9, 4, 20, 3, 10, 5};
+            var k2 = 33;
+            var output2 = 2;
+            // Action
+            var testCount1 = SubarraySumEqualsK.GetCumulativeSumCount(array1, k1);
+            var testCount2 = SubarraySumEqualsK.GetCumulativeSumCount(array2, k2);
+            // Assert
+            Assert.Equal(output1, testCount1);
+            Assert.Equal(output2, testCount2);
+        }
+    }
+}
