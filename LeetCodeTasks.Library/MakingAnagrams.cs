50cae8cb0bbc00f35b376b9b3dfaff5c910a0671
Alexandr Ageev <sashkaageev@gmail.com>
2022-03-15 01:53:30 +0300

    Added MakingAnagrams

---
+++ LeetCodeTasks.Library/MakingAnagrams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeTasks.Library
+{
+    public static class MakingAnagrams
+    {
+        public static int GetMakingAnagrams(string firstString, string secondString)
+        {
+            var array = new int[26];
+            
+            foreach (var ch in firstString)
+                array[ch - 97] += 1;
+
+            foreach (var ch in secondString)
+                array[ch - 97] -= 1;
+
+            return array.Sum(Math.Abs);
+        }
+    }
+}
---
+++ LeetCodeTasks.Tests/MakingAnagramsTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCodeTasks.Library;
+using Xunit;
+
+namespace LeetCodeTasks.Tests
+{
+    public class MakingAnagramsTests
+    {
+        [Fact]
+        public void Try_Execute_Making_Anagrams()
+        {
+            // Arrange
+            const string string1 = "cde";
+            const string string2 = "abc";
+            const int output1 = 4;
+            // Act
+            var result = MakingAnagrams.GetMakingAnagrams(string1, string2);
+            // Assert
+            Assert.Equal(output1, result);
+        }
+    }
+}
